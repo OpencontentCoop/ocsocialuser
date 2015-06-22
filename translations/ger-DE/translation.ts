eecb93e163505e9594a3e99eb57fba87d0e3c92a
Luca Realdi <luca.realdi@opencontent.it>
2015-06-22 12:05:10 +0200

    ger-DE

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>social_user/alerts</name>
+    <message>
+        <location filename="socialuser.php" line="250"/>
+        <source>Attiva il tuo profilo per partecipare!</source>
+        <translation>Aktivieren Sie Ihr Konto, um teilzunehmen!</translation>
+    </message>
+    <message>
+        <location filename="socialuser.php" line="252"/>
+        <source>Il tuo account è ora in moderazione, tutte le tue attività  non saranno rese pubbliche.</source>
+        <translation>Ihr Konto ist jetzt in Maßen, werden alle Ihre Aktivitäten nicht öffentlich gemacht werden.</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/mail/registration</name>
+    <message>
+        <source>Benvenuto in %1</source>
+        <translation>Willkommen in %1</translation>
+    </message>
+    <message>
+        <source>Grazie di voler partecipare!</source>
+        <translation>Vielen Dank für die Teilnahme</translation>
+    </message>
+    <message>
+        <source>Ecco le informazione del tuo profilo</source>
+        <translation>Informationen Ihres Profils</translation>
+    </message>
+    <message>
+        <source>Nome</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <source>Indirizzo email</source>
+        <translation>E-Mail-Adresse</translation>
+    </message>
+    <message>
+        <source>Clicca su questo link per confermare il tuo account</source>
+        <translation>Klicken Sie auf den Link, um Ihr Konto zu bestätigen</translation>
+    </message>
+    <message>
+        <source>Se desideri cambiare le impostazioni del tuo profilo clicca %profile_link_start%qui%profile_link_end%</source>
+        <translation>Profil bearbeiten - hier klicken %profile_link_start%qui%profile_link_end%</translation>
+    </message>
+    <message>
+        <source>Per abilitare o disabilitare le notifiche email clicca %notification_link_start%qui%notification_link_end%</source>
+        <translation>E-Mail-Benachrichtigungen aktivieren oder abmelden – hier klicken %notification_link_start%qui%notification_link_end%</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/setting</name>
+    <message>
+        <source>Username</source>
+        <translation>Benutzername</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Blocca utente</source>
+        <translation>Benutzer blockieren</translation>
+    </message>
+    <message>
+        <source>Impedisci all&apos;utente di commentare</source>
+        <translation>Verhindern Benutzer Kommentar</translation>
+    </message>
+    <message>
+        <source>Modera sempre le attività dell&apos;utente</source>
+        <translation>Gemäßigten immer User-Aktivitäten</translation>
+    </message>
+    <message>
+        <source>Permetti all&apos;utente di inserire segnalazioni per conto di altri</source>
+        <translation>So dass der Benutzer Berichte im Namen anderer eingeben</translation>
+    </message>
+    <message>
+        <source>Salva</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Annulla</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/signin</name>
+    <message>
+        <source>Per partecipare devi iscriverti!</source>
+        <translation>Zuerst registrieren, dann mitmachen!</translation>
+    </message>
+    <message>
+        <source>Accedi con il tuo account social</source>
+        <translation>Einloggen mit sozialem Netzwerk</translation>
+    </message>
+    <message>
+        <source>oppure</source>
+        <translation>oder</translation>
+    </message>
+    <message>
+        <source>Accedi con il tuo account</source>
+        <translation>Einloggen mit Ihrem Konto</translation>
+    </message>
+    <message>
+        <source>Sei già iscritto?</source>
+        <translation>Bereits registriert?</translation>
+    </message>
+    <message>
+        <source>Accedi subito!</source>
+        <translation>Sofort einloggen!</translation>
+    </message>
+    <message>
+        <source>Indirizzo Email</source>
+        <translation>Email Adresse</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Accedi</source>
+        <translation>Einloggen</translation>
+    </message>
+    <message>
+        <source>Resta collegato</source>
+        <translation>Eingeloggt bleiben</translation>
+    </message>
+    <message>
+        <source>Hai dimenticato la password?</source>
+        <translation>Password vergessen?</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/signup</name>
+    <message>
+        <location filename="socialuserregister.php" line="62"/>
+        <location filename="socialuserregister.php" line="80"/>
+        <location filename="socialuserregister.php" line="115"/>
+        <source>Inserire tutti i dati richiesti</source>
+        <translation>Nutzerdaten vollständig ausfüllen</translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="87"/>
+        <source>Indirizzo email non valido</source>
+        <translation>Email Adresse nicht gültig</translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="96"/>
+        <source>Email già  in uso. Hai dimenticato la password? &lt;a href=&quot;%forgot_url&quot;&gt;Clicca qui&lt;/a&gt;</source>
+        <translation>Email  bereits in Verwendung. Password vergessen? &lt;a href=&quot;%forgot_url&quot;&gt;klicken Sie hier&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="123"/>
+        <source>La password deve essere lunga almeno %1 caratteri</source>
+        <translation>Das Password muss mindestens %1 Zeichen lang sein</translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="132"/>
+        <source>La password non può essere &quot;password&quot;.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="151"/>
+        <source>Il nodo (%1) specificato in [UserSettings].DefaultUserPlacement setting in site.ini non esiste!</source>
+        <translation>Das Password darf nicht “Password” lauten</translation>
+    </message>
+    <message>
+        <location filename="socialuserregister.php" line="173"/>
+        <source>Configurazione errata di [UserSettings].UserObjectAttributeMap in social_user.ini</source>
+        <translation>Fehlkonfiguration [UserSettings].UserObjectAttributeMap (social_user.ini)</translation>
+    </message>
+    <message>
+        <source>Nome e cognome</source>
+        <translation>Vor- und Nachname</translation>
+    </message>
+    <message>
+        <source>Indirizzo Email</source>
+        <translation>E-Mail-Adresse</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Iscriviti</source>
+        <translation>Registrierung</translation>
+    </message>
+    <message>
+        <source>Codice di sicurezza</source>
+        <translation>Sicherheitscode</translation>
+    </message>
+    <message>
+        <source>reCAPTCHA API key non trovata</source>
+        <translation>reCAPTCHA API key du kannst nicht entzipfern</translation>
+    </message>
+    <message>
+        <source>Inserisci il codice di sicurezza</source>
+        <translation>Sicherheitscode eingeben</translation>
+    </message>
+    <message>
+        <source>Clicca qui per ottenere un nuovo codice</source>
+        <translation>Zeige einen neuen Code an</translation>
+    </message>
+    <message>
+        <source>Prosegui</source>
+        <translation>Weiter</translation>
+    </message>
+    <message>
+        <source>Ti è stata inviata un&apos;e-mail all&apos;indirizzo che hai specificato</source>
+        <translation>Sie haben eine E-Mail gesendet</translation>
+    </message>
+    <message>
+        <source>Segui le istruzioni che troverai nel messaggio per attivare il tuo profilo</source>
+        <translation>Folgen Sie den Anweisungen in dieser Mail, um Ihr Konto zu aktivieren</translation>
+    </message>
+    <message>
+        <source>Finché non avrai attivato il tuo profilo, tutte le tue attività saranno moderate</source>
+        <translation>Bis Sie Ihr Profil aktiviert haben, werden alle Ihre Aktivitäten moderat sein</translation>
+    </message>
+    <message>
+        <source>Inizia</source>
+        <translation>Starts</translation>
+    </message>
+    <message>
+        <source>Non sei ancora iscritto?</source>
+        <translation>Noch nicht registriert?</translation>
+    </message>
+    <message>
+        <source>Bastano 5 secondi per registrarsi!</source>
+        <translation>In 5 Sekunden bist du mit dabei!</translation>
+    </message>
+    <message>
+        <source>Crealo subito: &amp;egrave facile e gratuito!</source>
+        <translation>Schnell: Kostenlos registriert!</translation>
+    </message>
+    <message>
+        <source>Cliccando sul bottone Iscriviti accetti &lt;a href=%term_url&gt;le condizioni di utilizzo&lt;/a&gt; e confermi di aver letto la nostra &lt;a href=%privacy_url&gt;Normativa sull&apos;utilizzo dei dati&lt;/a&gt;.</source>
+        <translation>Klicke auf "Registrierung" und bestätige die &lt;a href=%term_url>Nutzungsbestimmungen&lt;/a&gt;. Bestätige die Datenschutzbestimmungen gelesen zu haben &lt;a href=%privacy_url>Privacy_URL&lt;/a&gt;.</translation>
+    </message>
+</context>
+</TS>
