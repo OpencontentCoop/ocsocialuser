79fadba3f9ace7d9dbce253ebeaa83c7c935261d
Luca Realdi <lr@opencontent.it>
2019-12-16 15:34:09 +0100

    Aggiunge il file di traduzione delle stringhe in greco

---
+++ translations/ell-GR/translation.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+  <context>
+    <name>social_user/signin</name>
+    <message>
+      <source>A valid username and password is required to log in.</source>
+      <translation>Ένα έγκυρο όνομα χρήστη και τον κωδικό πρόσβασης που απαιτείται για να συνδεθείτε.</translation>
+    </message>
+    <message>
+      <source>Access not allowed</source>
+      <translation>Πρόσβαση δεν επιτρέπεται</translation>
+    </message>
+    <message>
+      <source>Are you already a member?</source>
+      <translation>Είστε ήδη μέλος;</translation>
+    </message>
+    <message>
+      <source>Could not login</source>
+      <translation>Δεν ήταν δυνατή η σύνδεση</translation>
+    </message>
+    <message>
+      <source>Did you forget your password?</source>
+      <translation>Ξεχάσατε τον κωδικό σας?</translation>
+    </message>
+    <message>
+      <source>Email address</source>
+      <translation>Διεύθυνση ηλεκτρονικού ταχυδρομείου</translation>
+    </message>
+    <message>
+      <source>In order to partecipate, you need to be registered!</source>
+      <translation>Για να partecipate, πρέπει να είστε εγγεγραμμένος!</translation>
+    </message>
+    <message>
+      <source>Keep me logged in</source>
+      <translation>Να παραμείνω συνδεδεμένος</translation>
+    </message>
+    <message>
+      <source>Log in</source>
+      <translation>Σύνδεση</translation>
+    </message>
+    <message>
+      <source>Log in now!</source>
+      <translation>Συνδεθείτε τώρα!</translation>
+    </message>
+    <message>
+      <source>Login</source>
+      <translation>Σύνδεση</translation>
+    </message>
+    <message>
+      <source>login with your account</source>
+      <translation>συνδεθείτε με το λογαριασμό σας</translation>
+    </message>
+    <message>
+      <source>or</source>
+      <translation>ή</translation>
+    </message>
+    <message>
+      <source>Password</source>
+      <translation>Κωδικός πρόσβασης</translation>
+    </message>
+    <message>
+      <source>Remember me</source>
+      <translation>Θυμήσου με</translation>
+    </message>
+    <message>
+      <source>Sign in with your social account</source>
+      <translation>Συνδεθείτε με το κοινωνικό σας λογαριασμό</translation>
+    </message>
+    <message>
+      <source>You are not allowed to access% 1.</source>
+      <translation>Δεν επιτρέπεται η πρόσβαση% 1.</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/config</name>
+    <message>
+      <source>Add</source>
+      <translation>Προσθέτω</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/setting</name>
+    <message>
+      <source>Allow the user to enter alerts on behalf of other</source>
+      <translation>Αφήστε το χρήστη να εισάγει ειδοποιήσεις για λογαριασμό άλλων</translation>
+    </message>
+    <message>
+      <source>Always moderate user activities</source>
+      <translation>Πάντα μέτρια δραστηριοτήτων των χρηστών</translation>
+    </message>
+    <message>
+      <source>Block user</source>
+      <translation>Αποκλεισμός χρήστη</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Ματαίωση</translation>
+    </message>
+    <message>
+      <source>E-mail</source>
+      <translation>ΗΛΕΚΤΡΟΝΙΚΗ ΔΙΕΥΘΥΝΣΗ</translation>
+    </message>
+    <message>
+      <source>Prevent user to comment</source>
+      <translation>Αποτροπή χρήστης για να σχολιάσετε</translation>
+    </message>
+    <message>
+      <source>Save</source>
+      <translation>Σώσει</translation>
+    </message>
+    <message>
+      <source>Username</source>
+      <translation>Όνομα Χρήστη</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/signup</name>
+    <message>
+      <source>Are you not registered yet?</source>
+      <translation>Δεν έχετε εγγραφεί ακόμα;</translation>
+    </message>
+    <message>
+      <source>Click here</source>
+      <translation>Κάντε κλικ ΕΔΩ</translation>
+    </message>
+    <message>
+      <source>Click here to get a new code</source>
+      <translation>Κάντε κλικ εδώ για να πάρετε ένα νέο κωδικό</translation>
+    </message>
+    <message>
+      <source>Clicking the Subscribe button you accept &lt;a href=%term_url&gt;the terms of use&lt;/a&gt; and confirm that you have read our &lt;a href=%privacy_url&gt;Privacy Policy&lt;/a&gt;</source>
+      <translation>Κάνοντας κλικ στο κουμπί Εγγραφή δέχεστε &lt;a href=%term_url&gt; τους όρους χρήσης &lt;/a&gt; και επιβεβαιώστε ότι έχετε διαβάσει href=%privacy_url&gt; &lt;a μας Πολιτική Προστασίας Προσωπικών Δεδομένων &lt;/a&gt;</translation>
+    </message>
+    <message>
+      <source>Continue</source>
+      <translation>Να συνεχίσει</translation>
+    </message>
+    <message>
+      <source>Create an account now: it's easy and free!</source>
+      <translation>Δημιουργήστε ένα λογαριασμό τώρα: είναι εύκολο και δωρεάν!</translation>
+    </message>
+    <message>
+      <source>Email address</source>
+      <translation>Διεύθυνση ηλεκτρονικού ταχυδρομείου</translation>
+    </message>
+    <message>
+      <source>Email already in use. Did you forget your password?</source>
+      <translation>Email ήδη σε χρήση. Ξεχάσατε τον κωδικό σας?</translation>
+    </message>
+    <message>
+      <source>Email already in use. Did you forget your password? &lt;a href="%forgot_url"&gt; Click here &lt;/a&gt;</source>
+      <translation>Email ήδη σε χρήση. Ξεχάσατε τον κωδικό σας? &lt;a href="%forgot_url"&gt; Κάντε κλικ εδώ &lt;/a&gt;</translation>
+    </message>
+    <message>
+      <source>Enter all the requested data</source>
+      <translation>Εισάγετε όλα τα ζητούμενα στοιχεία</translation>
+    </message>
+    <message>
+      <source>Enter the security code</source>
+      <translation>Εισάγετε τον κωδικό ασφαλείας</translation>
+    </message>
+    <message>
+      <source>Follow the instructions in that mail to activate your account</source>
+      <translation>Ακολουθήστε τις οδηγίες σε αυτό το μήνυμα για την ενεργοποίηση του λογαριασμού σας</translation>
+    </message>
+    <message>
+      <source>Incorrect configuration of [UserSettings] .UserObjectAttributeMap in social_user.ini</source>
+      <translation>Εσφαλμένη ρύθμιση παραμέτρων του [UserSettings] .UserObjectAttributeMap σε social_user.ini</translation>
+    </message>
+    <message>
+      <source>Invalid e-mail address</source>
+      <translation>Μη έγκυρη διεύθυνση e-mail</translation>
+    </message>
+    <message>
+      <source>It takes just 5 seconds to register!</source>
+      <translation>Χρειάζονται μόλις 5 δευτερόλεπτα για να εγγραφείτε!</translation>
+    </message>
+    <message>
+      <source>Name and surname</source>
+      <translation>Ονομα και επίθετο</translation>
+    </message>
+    <message>
+      <source>Password</source>
+      <translation>Κωδικός πρόσβασης</translation>
+    </message>
+    <message>
+      <source>reCAPTCHA API key not found</source>
+      <translation>κλειδί API reCAPTCHA δεν βρέθηκε</translation>
+    </message>
+    <message>
+      <source>Security code</source>
+      <translation>Κωδικός ασφαλείας</translation>
+    </message>
+    <message>
+      <source>Start now</source>
+      <translation>Ξεκίνα τώρα</translation>
+    </message>
+    <message>
+      <source>Subscribe</source>
+      <translation>Εγγραφείτε</translation>
+    </message>
+    <message>
+      <source>The node (% 1) specified in [UserSettings] .DefaultUserPlacement setting in site.ini does not exist!</source>
+      <translation>Ο κόμβος (% 1) που ορίζονται στο [UserSettings] ρύθμιση .DefaultUserPlacement σε site.ini δεν υπάρχει!</translation>
+    </message>
+    <message>
+      <source>The password can not be "password."</source>
+      <translation>Ο κωδικός πρόσβασης δεν μπορεί να είναι «password».</translation>
+    </message>
+    <message>
+      <source>The password must be at least% 1 characters</source>
+      <translation>Ο κωδικός πρόσβασης πρέπει να είναι τουλάχιστον% 1 χαρακτήρες</translation>
+    </message>
+    <message>
+      <source>Until you have activated your profile, all of your activities will be moderate</source>
+      <translation>Μέχρι να ενεργοποιηθεί το προφίλ σας, όλες τις δραστηριότητές σας θα είναι μέτρια</translation>
+    </message>
+    <message>
+      <source>When they are validated you will receive an email notification to the email address you entered</source>
+      <translation>Όταν επικυρωθούν θα λάβετε μια ειδοποίηση μέσω ηλεκτρονικού ταχυδρομείου στη διεύθυνση ηλεκτρονικού ταχυδρομείου που εισαγάγατε</translation>
+    </message>
+    <message>
+      <source>You were sent an email to the address you specified</source>
+      <translation>Έχετε στείλει ένα μήνυμα ηλεκτρονικού ταχυδρομείου στη διεύθυνση που καθορίσατε</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/dashboard</name>
+    <message>
+      <source>Cancel</source>
+      <translation>Ματαίωση</translation>
+    </message>
+    <message>
+      <source>Delete items</source>
+      <translation>Διαγραφή στοιχείων</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/user_edit</name>
+    <message>
+      <source>Change password</source>
+      <translation>Αλλαξε κωδικό</translation>
+    </message>
+    <message>
+      <source>Change your password</source>
+      <translation>Αλλάξτε τον κωδικό σας</translation>
+    </message>
+    <message>
+      <source>E-mail</source>
+      <translation>ΗΛΕΚΤΡΟΝΙΚΗ ΔΙΕΥΘΥΝΣΗ</translation>
+    </message>
+    <message>
+      <source>Edit profile</source>
+      <translation>Επεξεργασία προφίλ</translation>
+    </message>
+    <message>
+      <source>Name</source>
+      <translation>Ονομα</translation>
+    </message>
+    <message>
+      <source>User profile</source>
+      <translation>Προφίλ χρήστη</translation>
+    </message>
+    <message>
+      <source>Username</source>
+      <translation>Όνομα Χρήστη</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/mail/registration</name>
+    <message>
+      <source>Click on the link to confirm your account</source>
+      <translation>Κάντε κλικ στο σύνδεσμο για να επιβεβαιώσετε το λογαριασμό σας</translation>
+    </message>
+    <message>
+      <source>Email address</source>
+      <translation>Διεύθυνση ηλεκτρονικού ταχυδρομείου</translation>
+    </message>
+    <message>
+      <source>If you want to change your profile settings Click %profile_link_start%here% profile_link_end%</source>
+      <translation>Αν θέλετε να αλλάξετε τις ρυθμίσεις του προφίλ σας Κάντε κλικ% profile_link_start% εδώ% profile_link_end%</translation>
+    </message>
+    <message>
+      <source>Name</source>
+      <translation>Ονομα</translation>
+    </message>
+    <message>
+      <source>Please find here your profile information</source>
+      <translation>Παρακαλούμε βρείτε τις πληροφορίες του προφίλ σας</translation>
+    </message>
+    <message>
+      <source>Thank you want to participate!</source>
+      <translation>Ευχαριστώ θέλετε να συμμετάσχετε!</translation>
+    </message>
+    <message>
+      <source>To enable or disable email notifications Click %notification_link_start% here% notification_link_end%</source>
+      <translation>Για να ενεργοποιήσετε ή να απενεργοποιήσετε τις ειδοποιήσεις ηλεκτρονικού ταχυδρομείου Κάντε κλικ% notification_link_start% εδώ% notification_link_end%</translation>
+    </message>
+    <message>
+      <source>Welcome to %1</source>
+      <translation>Καλώς ήλθατε στο% 1</translation>
+    </message>
+  </context>
+  <context>
+    <name>social_user/alerts</name>
+    <message>
+      <source>Enable your profile to participate!</source>
+      <translation>Ενεργοποιήστε το προφίλ σας να συμμετάσχουν!</translation>
+    </message>
+    <message>
+      <source>Your account is now in moderation, all your activities will not be made public.</source>
+      <translation>Ο λογαριασμός σας είναι τώρα με μέτρο, όλες οι δραστηριότητες σας δεν θα δημοσιοποιηθεί.</translation>
+    </message>
+  </context>
+</TS>
